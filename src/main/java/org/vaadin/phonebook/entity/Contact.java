2e29793de3fc1e0f15ff9bbf2a41b5a0d81c6a31
salman0336 <hafiz.salman@camp1.tkxel.com>
2023-02-13 16:11:21 +0500

    code formatting

--- src/main/java/org/vaadin/phonebook/entity/Contact.java
+++ src/main/java/org/vaadin/phonebook/entity/Contact.java
@@ -4,7 +4,7 @@ package org.vaadin.phonebook.entity;
 import java.time.LocalDateTime;
 
 
-public class Contact implements Cloneable {
+public class Contact{
 
 
     private String name;
@@ -14,83 +14,74 @@ public class Contact implements Cloneable {
     private String city;
     private String country;
     private String email;
-
     private LocalDateTime lastUpdatedTime;
 
-    public Contact(String name, String phoneNumber, String email, String street, String city, String country) {
-            this.name = name;
-            this.phoneNumber = phoneNumber;
-            this.street = street;
-            this.city = city;
-            this.country = country;
-            this.email = email;
-            lastUpdatedTime=LocalDateTime.now();
-        }
-        public String getName () {
-            return name;
-        }
-
-        public void setName (String name){
-            this.name = name;
-        }
-
-        public String getPhoneNumber () {
-            return phoneNumber;
-        }
-
-        public void setPhoneNumber (String phoneNumber){
-            this.phoneNumber = phoneNumber;
-        }
-
-        public String getStreet () {
-            return street;
-        }
-
-        public void setStreet (String street){
-            this.street = street;
-        }
-
-        public String getCity () {
-            return city;
-        }
-
-        public void setCity (String city){
-            this.city = city;
-        }
-
-        public String getCountry () {
-            return country;
-        }
-
-        public void setCountry (String country){
-            this.country = country;
-        }
-
-        public String getEmail () {
-            return email;
-        }
-
-        public void setEmail (String email){
-            this.email = email;
-        }
-        public LocalDateTime getLastUpdatedTime() {
-            return lastUpdatedTime;
-        }
-
-        public void setLastUpdatedTime(LocalDateTime lastUpdatedTime) {
-            this.lastUpdatedTime = lastUpdatedTime;
-        }
-
-        @Override
-        public Contact clone () { // NOSONAR
-            try {
-                return (Contact) super.clone();
-            } catch (CloneNotSupportedException e) {
-                return null;
-            }
-        }
     public Contact() {
-        }
-        ;
-
     }
+    public Contact(String name, String phoneNumber, String email, String street, String city, String country) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.street = street;
+        this.city = city;
+        this.country = country;
+        this.email = email;
+        lastUpdatedTime = LocalDateTime.now();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getLastUpdatedTime() {
+        return lastUpdatedTime;
+    }
+
+    public void setLastUpdatedTime(LocalDateTime lastUpdatedTime) {
+        this.lastUpdatedTime = lastUpdatedTime;
+    }
+
+}
